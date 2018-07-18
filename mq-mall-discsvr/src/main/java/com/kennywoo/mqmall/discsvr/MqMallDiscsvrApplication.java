3194b882939d0f80785ef9648531a3171ef9ff44
Kenny Woo <383230020@qq.com>
2018-07-19 05:28:31 +0800

    Initial commit

---
+++ mq-mall-discsvr/src/main/java/com/kennywoo/mqmall/discsvr/MqMallDiscsvrApplication.java
@@ -0,0 +1,12 @@
+package com.kennywoo.mqmall.discsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqMallDiscsvrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqMallDiscsvrApplication.class, args);
+    }
+}
