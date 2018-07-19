aa4401f195ba24a3a3f75770b5321d3d1aa71838
Kenny Woo <383230020@qq.com>
2018-07-19 16:17:48 +0800

    统一配置服务初始化设置

---
+++ mq-mall-confsvr/src/main/java/com/kennywoo/mqmall/confsvr/MqMallConfsvrApplication.java
@@ -0,0 +1,16 @@
+package com.kennywoo.mqmall.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableConfigServer
+@EnableEurekaClient
+@SpringBootApplication
+public class MqMallConfsvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqMallConfsvrApplication.class, args);
+	}
+}
---
+++ mq-mall-confsvr/src/test/java/com/kennywoo/mqmall/confsvr/MqMallConfsvrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kennywoo.mqmall.confsvr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MqMallConfsvrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
