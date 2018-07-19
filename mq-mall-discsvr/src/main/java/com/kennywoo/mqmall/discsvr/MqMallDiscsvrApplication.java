d328fad0f85bcf07c011a89f2cbaa58bc7920187
Kenny Woo <383230020@qq.com>
2018-07-19 15:45:11 +0800

    eureka 初始化设置

--- mq-mall-discsvr/src/main/java/com/kennywoo/mqmall/discsvr/MqMallDiscsvrApplication.java
+++ mq-mall-discsvr/src/main/java/com/kennywoo/mqmall/discsvr/MqMallDiscsvrApplication.java
@@ -2,7 +2,9 @@ package com.kennywoo.mqmall.discsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class MqMallDiscsvrApplication {
 
